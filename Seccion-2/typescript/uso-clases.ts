d41f3975afefcb08812658e678eb07d40ba7be8f
Farney Jiménez Toro <farney9@gmail.com>
2020-09-27 17:19:06 -0500

    Tipado de retorno

--- Seccion-2/typescript/uso-clases.ts
+++ Seccion-2/typescript/uso-clases.ts
@@ -1,56 +1,58 @@
 (() => {
-//  Clses en typescript
-    // class Avenger {
-    //     nombre: string;
-    //     equipo: string;
-    //     nombreReal: string;
-    //     puedePelear: boolean;
-    //     peleasGanadas: number;
-
-    //     constructor(
-    //         nombre: string, 
-    //         equipo: string, 
-    //         nombreReal?: string,
-    //         puedePelear: boolean,
-    //         peleasGanadas: number) {
-    //         this.nombre = nombre;
-    //         this.equipo = equipo;
-    //         this.nombreReal = nombreReal;
-    //         this.puedePelear = puedePelear;
-    //         this.peleasGanadas = peleasGanadas;
-            
-    //     }
-
-    // }
-
-    /*
-    Con esta nueva forma de declarar las propiedades de una clase
-    e inicializar sus valores se permite disminuir el elcódigo
-    incluso se puede indicar si las propiedades de clase son p¿ublicas
-    (accesibles dentro y fuera de la clase) oprivadas.
-    */
-    class Avenger {
-        // nombre: string;
-        // equipo: string;
-        // nombreReal: string;
-        // puedePelear: boolean;
-        // peleasGanadas: number;
-
-        constructor( 
-            public nomre: string,
-            public equipo: string,
-            public nombreReal?: string,
-            public puedePelear: boolean = true,
-            public peleasGanadas: number = 0) {
-
-            
-        }
-
-    }
-    const antman = new Avenger('Antman','Capi');
-    console.log(antman);
+    //  Clses en typescript
+        // class Avenger {
+        //     nombre: string;
+        //     equipo: string;
+        //     nombreReal: string;
+        //     puedePelear: boolean;
+        //     peleasGanadas: number;
     
-
-
-})();
+        //     constructor(
+        //         nombre: string, 
+        //         equipo: string, 
+        //         nombreReal?: string,
+        //         puedePelear: boolean,
+        //         peleasGanadas: number) {
+        //         this.nombre = nombre;
+        //         this.equipo = equipo;
+        //         this.nombreReal = nombreReal;
+        //         this.puedePelear = puedePelear;
+        //         this.peleasGanadas = peleasGanadas;
+                
+        //     }
+    
+        // }
+    
+        /*
+        Con esta nueva forma de declarar las propiedades de una clase
+        e inicializar sus valores se permite disminuir el elcódigo
+        incluso se puede indicar si las propiedades de clase son p¿ublicas
+        (accesibles dentro y fuera de la clase) oprivadas.
+        */
+        class Avenger {
+            // nombre: string;
+            // equipo: string;
+            // nombreReal: string;
+            // puedePelear: boolean;
+            // peleasGanadas: number;
+    
+            constructor( 
+                public nomre: string,
+                public equipo: string,
+                public nombreReal?: string,
+                public puedePelear: boolean = true,
+                public peleasGanadas: number = 0) {
+    
+                
+            }
+    
+        }
+        const antman = new Avenger('Antman','Capi');
+        console.log(antman);
+        
+    
+    
+    })();
+    
+    
 
