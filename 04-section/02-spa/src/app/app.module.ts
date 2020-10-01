48649ace3085fee934316ffb2066d6256bafb1be
Farney Jiménez Toro <farney9@gmail.com>
2020-10-01 13:59:57 -0500

    Add Routes

--- 04-section/02-spa/src/app/app.module.ts
+++ 04-section/02-spa/src/app/app.module.ts
@@ -1,14 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+// Servicios
 
+// Rutas
+import { AppRoutingModule } from './app.routes';
+
+// Components
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 04-section/02-spa/src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+
+
+const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'hero', component: HeroesComponent },
+    { path: 'about', component: AboutComponent },
+    // { path: '**', component: PageNotFoundComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home'}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
