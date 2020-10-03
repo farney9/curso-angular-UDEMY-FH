edb8b67c698e879f38f19dc57804aca047c60a3c
Farney Jiménez Toro <farney9@gmail.com>
2020-10-03 18:02:15 -0500

    Firts Commit Seccion 5 : Pipe

---
+++ 05-seccion/pipes/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre:string = 'Farney jiménez';
+
+  numbersArr =[1,2,3,4,5,6,7,8,9,10];
+
+  personajes:string[] = ['Ironman', 'Spiderman', 'Thor', 'Loki', 'Groot'];
+
+  PI:number = Math.PI;
+
+}
