e95b4dbab1849e206fc63b72e2a43f775d672185
Farney Jiménez Toro <farney9@gmail.com>
2020-10-09 11:18:39 -0500

    Diseño de pantalla y documentación ionic

--- 07-seccion/wishList/src/app/services/deseos.service.ts
+++ 07-seccion/wishList/src/app/services/deseos.service.ts
@@ -10,7 +10,9 @@ export class DeseosService {
 
   constructor() { 
 
-    console.log('Servicio inicializado');
-    
+    const lista1 = new Lista('Reunir gemas del infinito');
+    const lista2 = new Lista('Seleccionar héroes a desaparecer');
+    this.listas.push(lista1, lista2);
+
   }
 }
