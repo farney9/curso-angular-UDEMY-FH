b7717dd58736c5294b091084a3799c07c72ca233
Farney Jiménez Toro <farney9@gmail.com>
2020-10-17 10:14:48 -0500

    start project in Firebase import HttpClientModule

--- 10-login-app/authapp/src/app/app.module.ts
+++ 10-login-app/authapp/src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
+//Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -21,7 +24,8 @@ import { RegistroComponent } from './components/registro/registro.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule  
+    FormsModule,
+    HttpClientModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
