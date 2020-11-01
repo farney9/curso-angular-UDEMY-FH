9279efd7be0e51341dc65e2e29ab4277872396ad
Farney Jiménez Toro <farney9@gmail.com>
2020-10-31 22:48:22 -0500

    Create login component

--- 13-seccion/fireChat/src/app/app.module.ts
+++ 13-seccion/fireChat/src/app/app.module.ts
@@ -14,11 +14,13 @@ import { environment } from '../environments/environment';
 // Componentes
 import { ChatComponent } from './components/chat/chat.component';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChatComponent
+    ChatComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
