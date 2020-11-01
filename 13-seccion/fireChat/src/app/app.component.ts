ad7d4351f3c69f0ea9a0b670eabcf137ca242597
Farney Jiménez Toro <farney9@gmail.com>
2020-10-31 22:08:03 -0500

    checkpoint

--- 13-seccion/fireChat/src/app/app.component.ts
+++ 13-seccion/fireChat/src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 // importaciones para angularfire
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'fireChat';
   chats: Observable<any[]>;
+  
   constructor(firestore: AngularFirestore){
     this.chats = firestore.collection('chats').valueChanges();
   }
